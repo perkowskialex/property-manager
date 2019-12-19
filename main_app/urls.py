2e9772ec490fb780d85e482125a5e94420fe8400
Alex Perkowski <perkowskialex@gmail.com>
2019-12-19 09:30:58 -0600

    add templates

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('main_app.urls'))
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about')
 ]
