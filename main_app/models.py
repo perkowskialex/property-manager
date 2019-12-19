21e5b0e6e56f013aa3fd3e45e8ea055960461bcf
Alex Perkowski <perkowskialex@gmail.com>
2019-12-19 10:22:30 -0600

    set up model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Property(models.Model):
+    name = models.CharField(max_length = 100)
+    address = models.CharField(max_length = 200)
+    description = models.CharField(max_length = 500)
+    value = models.IntegerField()
--- main_app/views.py
+++ main_app/views.py
@@ -2,17 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-class Property:
-    def __init__(self, name, address, description, value):
-        self.name = name
-        self.address = address
-        self.description = description
-        self.value = value
-    
-properties = [
-    Property('House 1', '3204 Lafayette Ave', 'This house sucks!!', '450,000')
-]
-
 
 def home(request):
     return HttpResponse('<h1>HELLO</h1>')
